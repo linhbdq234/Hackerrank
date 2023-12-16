7ff209e4b908106f6781f4fa38e87b1fbe25631b
Nguyễn Thùy Linh <129659171+linhbdq234@users.noreply.github.com>
2023-12-16 17:13:06 +0700

    Add files via upload

---
+++ 1.21.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin >> a >> b >> c;
+	int tong1 = a+b+c;
+	int tong2= 2*(a+b);
+	int tong3= 2*(b+c);
+	int tong4= 2*(b+c);
+	cout << min({tong1,tong2,tong3,tong4});
+	return 0;
+}
